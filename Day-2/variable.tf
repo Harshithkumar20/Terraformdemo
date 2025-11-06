4174379a0a937f7483297c93c069899a7fee8fe3
Harshithkumar <harshithkumar820@gmail.com>
2025-11-06 16:29:08 +0530

    add

---
+++ Day-2/variable.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {
+    default = "ami-0157af9aea2eef346"
+    description = "ami for instance"
+  
+}
+
+variable "type" {
+    default = "t2.micro"
+    description = "type  for instance "
+}
