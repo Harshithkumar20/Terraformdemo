942501979292f3dd09036fccdd150b4bd9278a73
Harshithkumar <harshithkumar820@gmail.com>
2025-11-07 00:02:52 +0530

    day-2 added

---
+++ Day-2/output.tf
@@ -0,0 +1,13 @@
+output "publicip" {
+    value = aws_instance.name.public_ip
+  
+}
+output "AZ" {
+    value = aws_instance.name.availability_zone
+  
+}
+
+output "privateip" {
+    value =aws_instance.name.private_ip
+  
+}
--- Day-2/variable.tf
+++ Day-2/variable.tf
@@ -1,10 +1,12 @@
 variable "ami_id" {
-    default = "ami-0157af9aea2eef346"
+    default = " "
     description = "ami for instance"
+    type = string
   
 }
 
 variable "type" {
-    default = "t2.micro"
+    default = " "
     description = "type  for instance "
+    type = string
 }
