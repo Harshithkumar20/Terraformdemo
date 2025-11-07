641f0fd669305ec766f451b189402ce28c10e130
Harshithkumar <harshithkumar820@gmail.com>
2025-11-07 15:22:57 +0530

    added lifecycle

--- Day-4-state-locking/backend.tf
+++ Day-4-state-locking/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket = "harshithkumar-terraform-bucket-2025"
     key    = "terraform.tfvars"
+    use_lockfile = true #to lock the statefile so that till my changes apply no other person can make changes 
     region = "us-east-1"
   }
 }
